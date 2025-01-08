614e01af15b044c2aa5ac3031b2bb84b6e67c1fc
unknown <dhanya13md@gmail.com>
2025-01-09 00:29:22 +0530

    callback routes

---
+++ src/app/callback/complete/page.tsx
@@ -0,0 +1,24 @@
+import { onSignupUser } from "@/app/actions/auth"
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+const CompleteOAuthAfterCallback = async () => {
+  const user = await currentUser()
+  if (!user) redirect("/sign-in")
+  const complete = await onSignupUser({
+    firstname: user.firstName as string,
+    lastname: user.lastName as string,
+    image: user.imageUrl,
+    clerkId: user.id,
+  })
+
+  if (complete.status == 200) {
+    redirect(`/group/create`)
+  }
+
+  if (complete.status !== 200) {
+    redirect("/sign-in")
+  }
+}
+
+export default CompleteOAuthAfterCallback
