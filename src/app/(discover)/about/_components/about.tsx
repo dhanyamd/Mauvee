767d92d376ee86266e09e0a77108919dd583657a
unknown <dhanya13md@gmail.com>
2025-02-06 23:19:44 +0530

    group-info-about hook

--- src/app/(discover)/about/_components/about.tsx
+++ src/app/(discover)/about/_components/about.tsx
@@ -1,4 +1,7 @@
+'use client'
+import { NoResult } from '@/app/globals/search/no-result'
 import { useGroupInfo } from '@/hooks/groups'
+import { useGroupAbout } from '@/hooks/groups/groupcreate'
 import React from 'react'
 
 type Props = {
@@ -8,11 +11,75 @@ type Props = {
 
 const AboutGroup = ({groupid, userid} : Props) => {
     const { group } = useGroupInfo()
+    const {
+      activeMedia, 
+      editor,
+      errors,
+      isPending, 
+      onDescription,
+      onEditDescription,
+      onJsonDescription,
+      onSetActionMedia,
+      onUpdateDescription,
+      setJsonDescription,
+      setOnDescription, setOnHtmlDescription
+    } = useGroupAbout( group.description, group.jsonDescription, group.htmlDescription, group.gallery[0], groupid)
+
+  if(!group)
+    return (
+  <div>
+    <NoResult />
+  </div>)
+
   return (
-    <div>
-      
-    </div>
+   <div className='flex flex-col gap-y-10'>
+   <div>
+    <h2 className='font-bold text-[56px] leading-none md:leading-normal'>
+    {group.name}
+    </h2>
+    <p className='text-themeTextGray leading-none md:mt-2 mt-5'>
+     {group.description}
+    </p>
+   </div>
+   {group.gallery.length > 0 && (
+    <div className='relative rounded-xl'>
+      <div className='img--overlay absolute h-2/6 bottom-0 w-full z-50'>
+        {activeMedia?.type === "IMAGE" ? (
+          <img 
+          src={`https://ucarecdn.com/${activeMedia.url}/`}
+          alt='group-img'
+          className='w-full aspect-video z-20 rounded-t-xl'
+          />
+        ) :
+         activeMedia?.type === "LOOM" ? (
+          <div className='w-full aspect-video'>
+            <iframe
+            src={activeMedia.url}
+            allowFullScreen
+            className='absolute outline-none border-0 top-0 left-0 w-full h-full rounded-t-xl'
+            >
+
+            </iframe>
+            </div>
+         ) : (
+          activeMedia?.type === "YOUTUBE" && (
+            <div className='w-full aspect-video relative'>
+              <iframe 
+              allowFullScreen
+              src={activeMedia.url}
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              className='w-full absolute top-0 left-0 h-full rounded-xl'>
+              </iframe>
+              </div>
+          )
+         )
+        }
+        </div>
+</div>
+   )}
+   </div>
   )
+  
 }
 
 export default AboutGroup
--- src/hooks/groups/groupcreate.ts
+++ src/hooks/groups/groupcreate.ts
@@ -1,12 +1,15 @@
-import { onCreateNewGroup } from "@/app/actions/groups"
+import { onCreateNewGroup, onUpDateGroupSettings } from "@/app/actions/groups"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 import { CreateGroupSchema } from "../schema"
 import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { toast } from "sonner"
 import { z } from "zod"
 import { useMutation } from "@tanstack/react-query"
+import { validateURLString } from "@/lib/utils"
+import { JSONContent } from "novel"
+import { GroupSettingsSchema } from "@/components/forms/group-setttings/schema"
 
 export const useGroup = async(
     userId: string,
@@ -67,5 +70,148 @@ return {
   register,
   errors,
   isCategory,
-
 }}
+
+export const useGroupAbout = (
+  description : string | null,
+  jsonDescription : string | null,
+  htmlDescription : string | null,
+  currentMedia : string,
+  groupid : string
+) => {
+  const editor = useRef<HTMLFormElement | null>(null)
+  const mediaType = validateURLString(currentMedia)
+  const [activeMedia, setActiveMedia] = useState< | {
+    url : string | undefined
+    type : string 
+  }
+  | undefined
+  >(
+    mediaType.type === "IMAGE" ? {
+      url : currentMedia,
+      type : mediaType.type
+    } : { ...mediaType},
+  )
+
+  const jsonContent = jsonDescription !== null ? JSON.parse(jsonDescription as string) : undefined
+
+  const [onJsonDescription, setJsonDescription] = useState<JSONContent | undefined>(jsonContent)
+  const [onDescription, setOnDescription] = useState<string | undefined>(description || undefined)
+  const [onHtmlDescription, setOnHtmlDescription] = useState<string | undefined>(htmlDescription || undefined)
+  const [onEditDescription, setOnEditDescription] = useState<boolean>(false)
+
+  const {
+    setValue,
+    formState: {errors},
+    handleSubmit
+  } = useForm<z.infer<typeof GroupSettingsSchema>>({
+    resolver: zodResolver(GroupSettingsSchema)
+  })
+  
+const onSetDescriptions = () => {
+  const JsonContent = JSON.stringify(onJsonDescription)
+  setValue("jsondescription", JsonContent)
+  setValue("description", onDescription)
+  setValue("htmldescription", onHtmlDescription)
+}
+
+  useEffect(() => {
+    onSetDescriptions()
+    return () => {
+      onSetDescriptions()
+    }
+  }, [onJsonDescription, onDescription])
+
+ const onEditTextEditor = (event : Event) => {
+  if(editor.current){
+    !editor.current.contains(event.target as Node | null)
+    ? setOnEditDescription(false)
+    : setOnEditDescription(true)
+  }
+ }
+
+  useEffect(() => {
+    document.addEventListener("click", onEditTextEditor, false)
+    return () => {
+      document.removeEventListener("click", onEditTextEditor), false
+    }
+  },[])
+  //optimistic ui => useMutation 
+  const {mutate, isPending} = useMutation({
+     mutationKey : ["about-description"],
+     mutationFn: async (values : z.infer<typeof GroupSettingsSchema>) => {
+      if(values.description) {
+        const updated = await onUpDateGroupSettings(
+          groupid,
+          "DESCRIPTION",
+          values.description,
+          `/about/${groupid}`
+        )
+        if(updated.status !== 200){
+          return toast("Error", {
+           description: "Oops! looks like your form is empty"
+          })
+        }
+      }
+      if(values.jsondescription){
+        const updated = await onUpDateGroupSettings(
+          groupid,
+          "JSONDESCRIPTION",
+          values.jsondescription,
+          `/about/${groupid}`
+        )
+        if(updated.status !== 200) {
+          return toast("Error", {
+            description: "Oops! looks like your form is empty"
+          })
+        }
+      }
+      if(values.htmldescription){
+        const updated = await onUpDateGroupSettings(
+          groupid,
+          "HTMLDESCRIPTION",
+          values.htmldescription,
+          `/about/${groupid}`
+        )
+        if(updated.status !== 200) {
+          return toast("Error", {
+            description: "Oops! looks like your form is empty"
+          })
+        }
+     }
+     if(
+      !values.description && 
+      !values.jsondescription && 
+      !values.htmldescription
+     ) {
+      return toast("Error", {
+        description: "Oops! looks like your form is empty"
+      })
+     }
+     return toast("Success", {
+      description : "Group description updated"
+     })
+    }
+  })
+  const onSetActionMedia = (media : { url : string | undefined; type : string}) => 
+    setActiveMedia(media)
+
+  const onUpdateDescription = handleSubmit(async (values) => {
+    mutate(values)
+  })
+
+  return {
+    setOnDescription,
+    onDescription,
+    setJsonDescription,
+    onJsonDescription,
+    errors,
+    onEditDescription,
+    editor,
+    activeMedia,
+    onSetActionMedia,
+    setOnHtmlDescription,
+   onUpdateDescription,
+   isPending
+  }
+}
