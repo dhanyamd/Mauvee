975e072e34e3b1b37e64b802e4d6168cfaf3f47d
unknown <dhanya13md@gmail.com>
2025-02-08 17:41:41 +0530

    simple modal

---
+++ src/app/globals/simple-modal/index.tsx
@@ -0,0 +1,71 @@
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+import { ArrowLeft, ArrowRight } from "lucide-react"
+import Image from "next/image"
+
+type SimpleModalProps = {
+    trigger: JSX.Element
+    children : React.ReactNode
+    title? : string
+    description? : string 
+    type? : "Integration"
+    logo? : string
+}
+
+export const SimpleModal = ({
+ trigger,
+ children,
+ title,
+ type,
+ logo,
+ description
+} : SimpleModalProps) => {
+    switch (type) {
+        case "Integration":
+            return (
+             <Dialog>
+                <DialogTrigger asChild>{trigger}</DialogTrigger>
+                <DialogContent className="bg-themeBlack border-themeDarkGray">
+                <div className="flex justify-center gap-3">
+                <div className="w-12 h-12 relative">
+                  <Image 
+                  src={`https://ucarecdn.com/9c03e259-4d78-4f22-afec-79f0cb6831c8/gradient2.jpg`}
+                  fill
+                  alt="Corinna"
+                  />
+                </div>
+                <div className="text-gray-400">
+                   <ArrowLeft size={20} />
+                   <ArrowRight size={20}/>
+                </div>
+                <div className="w-12 h-12 relative">
+                   <Image 
+                   src={`https://ucarecdn.com/${logo}/`}
+                   fill
+                   alt="stripe"
+                   />
+                </div>
+                </div>
+                <DialogHeader className="flex items-center">
+             <DialogTitle className="text-xl">
+              {title}
+             </DialogTitle>
+             <DialogDescription className="text-center">{description}</DialogDescription>
+                </DialogHeader>
+                {children}
+                </DialogContent>
+             </Dialog>
+            )
+    
+        default:
+            return (
+                <Dialog>
+                    <DialogTrigger asChild>{trigger}</DialogTrigger>
+                    <DialogContent className="bg-[#1C1C1E] !max-w-2xl border-themeGray">
+                       {children}
+                    </DialogContent>
+                </Dialog>
+            )
+    }
+  
+}
+
--- src/app/group/[groupid]/channel/[channelid]/_components/create-post/index.tsx
+++ src/app/group/[groupid]/channel/[channelid]/_components/create-post/index.tsx
@@ -1,4 +1,7 @@
 'use client'
+import { SimpleModal } from '@/app/globals/simple-modal';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import { Card, CardContent, CardDescription } from '@/components/ui/card';
 import { useChannelPage } from '@/hooks/channels';
 import React from 'react'
 
@@ -8,9 +11,36 @@ const CreateNewPost = ({userImage, channelid, username}: Props) => {
     const {data, mutation} = useChannelPage(channelid)
     const {name} = data as {name : string}
   return (
-    <div>
-      
-    </div>
+   <>
+      <SimpleModal trigger={
+        <span>
+            <Card className='border-themeGray cursor-pointer first-letter:rounded-2xl overflow-hidden'>
+             <CardContent className='p-3 bg-[#1A1A1D] flex gap-x-6 items-center'>
+              <Avatar className='cursor-pointer'>
+                <AvatarImage src={userImage} alt='user' />
+                <AvatarFallback>U</AvatarFallback>
+              </Avatar>
+              <CardDescription className='text-themeTextGray'>
+                   Type / to add elements to your post... 
+              </CardDescription>
+             </CardContent>
+            </Card>
+        </span>
+      }>
+         <div className='flex gap-x-3'>
+        <Avatar className='cursor-pointer'>
+            <AvatarImage src={userImage} alt='user'/>
+            <AvatarFallback>U</AvatarFallback>
+        </Avatar>
+        <div className='flex flex-col'>
+       <p className='text-themeTextGray text-sm capitalize'>{username}</p>
+       <p className='text-sm capitalize text-themeTextGray'>Posting in{" "}</p>
+       <span className='font-bold capitalize text-themeTextWhite '>{name}</span>
+        </div>
+         </div>
+      </SimpleModal>
+   
+   </>
   )
 }
 
