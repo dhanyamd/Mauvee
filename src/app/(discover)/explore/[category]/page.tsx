5f66474adbdd3e69e68cbbff941b94f726218f1f
unknown <dhanya13md@gmail.com>
2025-02-28 20:43:36 +0530

    fix lol

--- src/app/(discover)/explore/[category]/page.tsx
+++ src/app/(discover)/explore/[category]/page.tsx
@@ -2,22 +2,22 @@ import { onGetExploreGroup } from "@/app/actions/groups"
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query"
 import ExplorePageContent from "../_components/explore-content"
 
+type CategoryProps = { 
+    params: Promise<{ category: string; }>; 
+  };
 
-export default async function CategoryPage ({
-    params,
-} : {
-    params : {category : string}
-}){
+export default async function CategoryPage ({params} : CategoryProps ){
+    const { category } = await params;
     const query = new QueryClient()
 
     await query.prefetchQuery({
         queryKey: ["groups"],
-        queryFn: () => onGetExploreGroup(params.category, 0)
+        queryFn: () => onGetExploreGroup(category, 0)
     })
 
     return (
         <HydrationBoundary state={dehydrate(query)}>
-        <ExplorePageContent layout="LIST" category={params.category}/>
+        <ExplorePageContent layout="LIST" category={category}/>
         </HydrationBoundary>
     )
 }
